444fa6f192a3deb1c5b1f35409f09a3bb3e95abf
Martijn Cassels <martijncassels@gmail.com>
2018-10-15 14:51:31 +0200

    fixed router 15-10-2018

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,7 +11,7 @@ angular
 
 MainCtrl.$inject = ['$scope','$http','_','$interval'];
 HomeCtrl.$inject = ['$scope', '$route','$http','$interval','Helpers','moment'];
-PackingCtrl.$inject = ['$scope', '$route','$http','$interval','Helpers','moment'];
+PackingCtrl.$inject = ['$scope', '$route', '$routeParams', '$http','$interval','Helpers','moment'];
 
 function MainCtrl($scope,$http,_,$interval) {
 	var vm = this;
@@ -28,24 +28,33 @@ function HomeCtrl($scope,$route,$http,$interval,Helpers,moment) {
 
 }
 
-function PackingCtrl($scope,$route,$http,$interval,Helpers,moment) {
+function PackingCtrl($scope,$route,$routeParams,$http,$interval,Helpers,moment) {
 		var vm = this;
-		vm.title = '';
+		vm.title = 'Packing';
 
-		$http.get('/api/packing2018/100/0/0/5')
-				.success(function(data) {
-						data = Helpers.parseTimestamps(data);
-						angular.forEach(data, function(value,index){
-							value.Time = moment.utc(value.Time).format('HH:mm')
-							value.ShipDate = moment.utc(value.ShipDate).format('YYYY-MM-DD');
-						});
-						vm.data = data;
-				})
-				.error(function(data) {
-						console.log('Error: ' + data);
-						vm.error = data;
-				});
+		vm.getPacking = function(){
+			$http.get('/api/packing2018/'+$routeParams.topRows+'/'+$routeParams.transportType+'/'+$routeParams.daysAhead+'/'+$routeParams.daysIncluded)
+					.success(function(data) {
+							data = Helpers.parseTimestamps(data);
+							angular.forEach(data, function(value,index){
+								value.Time = moment.utc(value.Time).format('HH:mm')
+								value.ShipDate = moment.utc(value.ShipDate).format('YYYY-MM-DD');
+							});
+							vm.data = data;
+					})
+					.error(function(data) {
+							console.log('Error: ' + data);
+							vm.error = data;
+					});
+			}
 
+		vm.getPacking();
+
+		var packinginterval = $interval(function () {vm.getPacking()}, 60000);
+
+		$scope.$on('$destroy', function() {
+			$interval.cancel(packinginterval);
+		});
 }
 
 // helpers
